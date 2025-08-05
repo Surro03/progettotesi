87494f5494ea4556798a07601bd08af2aa940a4a
Surro03 <samuele2003@icloud.com>
2025-08-05 14:59:49 +0200

    First commit

---
+++ src/main/java/it/univaq/progettotesi/ProgettoTesiApplication.java
@@ -0,0 +1,13 @@
+package it.univaq.progettotesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoTesiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoTesiApplication.class, args);
+    }
+
+}
