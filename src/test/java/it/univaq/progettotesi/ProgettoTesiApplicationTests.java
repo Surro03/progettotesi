e5e76b0e7647f89fb6c6caf6fb05359562fd2947
Lucass02-02 <luave2002@gmail.com>
2025-08-05 12:34:36 +0200

    primo commit

---
+++ src/test/java/it/univaq/progettotesi/ProgettoTesiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univaq.progettotesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoTesiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
